c312b0baaeb58c74fdcb8f7305eab7dd06ac4bf5
Zhaiyu Chen <zhaiyu.chen@outlook.com>
2021-11-30 16:04:24 +0100

    Copy identified changed rasters for inspection

--- compare.py
+++ compare.py
@@ -5,13 +5,13 @@ Compare AHN 3/4 raster.
 import multiprocessing
 from pathlib import Path
 import logging
+import shutil
 
 import hydra
 import numpy as np
 from osgeo import gdal
 from tqdm import tqdm
 from omegaconf import DictConfig
-
 
 log = logging.getLogger(__name__)
 
@@ -30,6 +30,7 @@ def difference(path_reference, path_target):
     if (reference.RasterXSize == 1 and reference.RasterYSize == 1) or \
             (target.RasterXSize == 1 and target.RasterYSize == 1):
         # empty tif
+        # this is no longer useful for the updated geoflow where footprint is used to determine the image size
         return np.array([0])
 
     # they should have identical dimension to compare
@@ -53,30 +54,53 @@ def difference(path_reference, path_target):
     return np.absolute(array_target - array_reference)
 
 
-def change(difference_array, path_reference, **kwargs):
+def changed(difference_array, **kwargs):
     """
     Verdict if the building is changed.
     :param difference_array: array of difference
-    :param path_reference: path to reference
     :return: True/False
     """
-    changed = {}
+    changed_ = {}
     if 'mean' in kwargs:
         mean = np.mean(difference_array)
-        changed['mean'] = True if mean > kwargs['mean'] else False
+        changed_['mean'] = True if mean > kwargs['mean'] else False
     if 'maxima' in kwargs:
         maxima = np.max(difference_array)
-        changed['maxima'] = True if maxima > kwargs['maxima'] else False
+        changed_['maxima'] = True if maxima > kwargs['maxima'] else False
     if 'sum' in kwargs:
         sum_ = np.sum(difference_array)
-        changed['sum'] = True if sum_ > kwargs['sum'] else False
+        changed_['sum'] = True if sum_ > kwargs['sum'] else False
     if 'count_larger_than' in kwargs:
         count_larger_than = np.sum(difference_array > kwargs['count_larger_than'][0])
-        changed['count_larger_than'] = True if count_larger_than > kwargs['count_larger_than'][1] else False
+        changed_['count_larger_than'] = True if count_larger_than > kwargs['count_larger_than'][1] else False
     if 'percentage_larger_than' in kwargs:
         percentage_larger_than = np.sum(difference_array > kwargs['percentage_larger_than'][0]) / difference_array.size
-        changed['percentage_larger_than'] = True if percentage_larger_than > kwargs['percentage_larger_than'][1] else False
-    return changed, path_reference
+        changed_['percentage_larger_than'] = True if percentage_larger_than > kwargs['percentage_larger_than'][
+            1] else False
+    return changed_
+
+
+def copy_file(path_reference, path_target, reference_dir, target_dir, save_dir):
+    """
+    Copy reference and target to specified dir if they are changed.
+    :return: None
+    """
+    # create dir structure as tile_number/raster_number
+    path_reference_copy = save_dir / path_reference.relative_to(reference_dir).with_suffix('.reference.tif')
+    path_target_copy = save_dir / path_target.relative_to(target_dir).with_suffix('.target.tif')
+
+    path_reference_copy.parent.mkdir(exist_ok=True, parents=True)
+    path_target_copy.parent.mkdir(exist_ok=True, parents=True)
+
+    try:
+        shutil.copyfile(path_reference, path_reference_copy)
+        shutil.copyfile(path_target, path_target_copy)
+    except shutil.SameFileError:
+        log.error('Source and destination represents the same file.')
+    except IsADirectoryError:
+        log.error('Destination is a directory.')
+    except PermissionError:
+        log.error('Permission denied.')
 
 
 def compare(args):
@@ -85,9 +109,13 @@ def compare(args):
     :param args: path_reference, path_target
     :return: whether the building is changed
     """
-    path_reference, path_target, metrics = args
+    path_reference, path_target, reference_dir, target_dir, save_dir, metrics = args
     difference_array = difference(path_reference, path_target)
-    return change(difference_array, path_reference, **metrics)
+    changed_ = changed(difference_array, **metrics)
+
+    if True in changed_.values():
+        copy_file(path_reference, path_target, reference_dir, target_dir, save_dir)
+        return changed_, path_reference
 
 
 @hydra.main(config_path='conf', config_name='config')
@@ -103,25 +131,19 @@ def multi_run(cfg: DictConfig):
     args = []
     for path_reference in reference_paths:
         path_target = target_dir / path_reference.relative_to(reference_dir).with_suffix('.tif')
-        args.append((path_reference, path_target, cfg.compare.metrics))
+        args.append((path_reference, path_target, cfg.compare.reference_dir, cfg.compare.target_dir,
+                     cfg.compare.save_dir, cfg.compare.metrics))
 
     pool = multiprocessing.Pool(processes=cfg.compare.threads if cfg.compare.threads else multiprocessing.cpu_count())
 
-    counter = 0
     # https://stackoverflow.com/a/40133278
-    for c, filepath in tqdm(pool.imap_unordered(compare, args), total=len(args)):
-        if True in c.values():
+    counter = 0
+    for r in tqdm(pool.imap_unordered(compare, args), total=len(args)):
+        if r is not None:
             counter += 1
-            log.info(str(c) + str(filepath))
-    log.info(f' {counter} / {len(args)} has changed')
-
-    # fallback to single-thread
-    # for arg in args:
-    #     c, filepath = compare(arg)
-    #     if True in c.values():
-    #         counter += 1
-    #         log.info(str(c) + str(filepath))
-    # log.info(f' {counter} / {len(args)} has changed')
+            if cfg.compare.verbose:
+                log.info(str(r[0]) + ' for ' + str(r[1]))
+    log.info(f' {counter} / {len(args)} has changed.')
 
 
 if __name__ == '__main__':
