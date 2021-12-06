818f3e455ded081745292c23506ff90b3291fb63
Zhaiyu Chen <zhaiyu.chen@outlook.com>
2021-12-06 15:22:15 +0100

    class RasterComparison with las

--- compare.py
+++ compare.py
@@ -12,110 +12,134 @@ import numpy as np
 from osgeo import gdal
 from tqdm import tqdm
 from omegaconf import DictConfig
+from hydra.utils import instantiate
 
 log = logging.getLogger(__name__)
 
 
-def difference(path_reference, path_target):
-    """
-    Compute pixel-wise absolute difference between two raster.
-    No-data pixels are ignored.
-    :param path_reference: path to reference raster
-    :param path_target: path to target raster
-    :return: difference
-    """
-    reference = gdal.Open(str(path_reference))
-    target = gdal.Open(str(path_target))
+class RasterComparison:
+    def __init__(self, **kwargs):
+        self.reference_tif_dir = kwargs['reference_tif_dir']
+        self.target_tif_dir = kwargs['target_tif_dir']
+        self.save_tif_dir = kwargs['save_tif_dir']
 
-    if (reference.RasterXSize == 1 and reference.RasterYSize == 1) or \
-            (target.RasterXSize == 1 and target.RasterYSize == 1):
-        # empty tif
-        # this is no longer useful for the updated geoflow where footprint is used to determine the image size
-        return np.array([0])
+        self.reference_las_dir = kwargs['reference_las_dir']
+        self.target_las_dir = kwargs['target_las_dir']
+        self.save_las_dir = kwargs['save_las_dir']
 
-    # they should have identical dimension to compare
-    assert reference.RasterXSize == target.RasterXSize and \
-           reference.RasterYSize == target.RasterYSize
+        self.metrics = kwargs['metrics']
 
-    band_reference = reference.GetRasterBand(1)
-    array_reference = band_reference.ReadAsArray()
+    @staticmethod
+    def difference(path_reference, path_target):
+        """
+        Compute pixel-wise absolute difference between two raster.
+        No-data pixels are ignored.
+        :param path_reference: path to reference raster
+        :param path_target: path to target raster
+        :return: difference
+        """
+        reference = gdal.Open(str(path_reference))
+        target = gdal.Open(str(path_target))
 
-    band_target = target.GetRasterBand(1)
-    array_target = band_target.ReadAsArray()
+        if (reference.RasterXSize == 1 and reference.RasterYSize == 1) or \
+                (target.RasterXSize == 1 and target.RasterYSize == 1):
+            # empty tif
+            # this is no longer useful for the updated geoflow where footprint is used to determine the image size
+            return np.array([0])
 
-    nodata_reference = band_reference.GetNoDataValue()
-    nodata_target = band_target.GetNoDataValue()
+        # they should have identical dimension to compare
+        assert reference.RasterXSize == target.RasterXSize and \
+               reference.RasterYSize == target.RasterYSize
 
-    # zero the nodata pixels for both raster
-    nodata_mask = np.bitwise_or(array_reference == nodata_reference, array_target == nodata_target)
-    array_reference[nodata_mask] = 0
-    array_target[nodata_mask] = 0
+        band_reference = reference.GetRasterBand(1)
+        array_reference = band_reference.ReadAsArray()
 
-    return np.absolute(array_target - array_reference)
+        band_target = target.GetRasterBand(1)
+        array_target = band_target.ReadAsArray()
 
+        nodata_reference = band_reference.GetNoDataValue()
+        nodata_target = band_target.GetNoDataValue()
 
-def changed(difference_array, **kwargs):
-    """
-    Verdict if the building is changed.
-    :param difference_array: array of difference
-    :return: True/False
-    """
-    changed_ = {}
-    if 'mean' in kwargs:
-        mean = np.mean(difference_array)
-        changed_['mean'] = True if mean > kwargs['mean'] else False
-    if 'maxima' in kwargs:
-        maxima = np.max(difference_array)
-        changed_['maxima'] = True if maxima > kwargs['maxima'] else False
-    if 'sum' in kwargs:
-        sum_ = np.sum(difference_array)
-        changed_['sum'] = True if sum_ > kwargs['sum'] else False
-    if 'count_larger_than' in kwargs:
-        count_larger_than = np.sum(difference_array > kwargs['count_larger_than'][0])
-        changed_['count_larger_than'] = True if count_larger_than > kwargs['count_larger_than'][1] else False
-    if 'percentage_larger_than' in kwargs:
-        percentage_larger_than = np.sum(difference_array > kwargs['percentage_larger_than'][0]) / difference_array.size
-        changed_['percentage_larger_than'] = True if percentage_larger_than > kwargs['percentage_larger_than'][
-            1] else False
-    return changed_
+        # zero the nodata pixels for both raster
+        nodata_mask = np.bitwise_or(array_reference == nodata_reference, array_target == nodata_target)
+        array_reference[nodata_mask] = 0
+        array_target[nodata_mask] = 0
 
+        return np.absolute(array_target - array_reference)
 
-def copy_tif(path_reference, path_target, reference_dir, target_dir, save_dir):
-    """
-    Copy reference and target to specified dir if they are changed.
-    :return: None
-    """
-    # create dir structure as tile_number/raster_number
-    path_reference_copy = save_dir / path_reference.relative_to(reference_dir).with_suffix('.reference.tif')
-    path_target_copy = save_dir / path_target.relative_to(target_dir).with_suffix('.target.tif')
+    def changed(self, difference_array):
+        """
+        Verdict if the building is changed.
+        :param difference_array: array of difference
+        :return: True/False
+        """
+        changed_ = {}
+        if 'mean' in self.metrics:
+            mean = np.mean(difference_array)
+            changed_['mean'] = True if mean > self.metrics['mean'] else False
+        if 'maxima' in self.metrics:
+            maxima = np.max(difference_array)
+            changed_['maxima'] = True if maxima > self.metrics['maxima'] else False
+        if 'sum' in self.metrics:
+            sum_ = np.sum(difference_array)
+            changed_['sum'] = True if sum_ > self.metrics['sum'] else False
+        if 'count_larger_than' in self.metrics:
+            count_larger_than = np.sum(difference_array > self.metrics['count_larger_than'][0])
+            changed_['count_larger_than'] = True if count_larger_than > self.metrics['count_larger_than'][1] else False
+        if 'percentage_larger_than' in self.metrics:
+            percentage_larger_than = np.sum(
+                difference_array > self.metrics['percentage_larger_than'][0]) / difference_array.size
+            changed_['percentage_larger_than'] = True if percentage_larger_than > \
+                                                         self.metrics['percentage_larger_than'][
+                                                             1] else False
+        return changed_
 
-    path_reference_copy.parent.mkdir(exist_ok=True, parents=True)
-    path_target_copy.parent.mkdir(exist_ok=True, parents=True)
+    def copy_file(self, path_reference, path_target, filetype):
+        """
+        Copy reference and target to specified dir if they are changed.
+        :return: None
+        """
+        # create dir structure as tile_number/raster_number
+        stem = path_reference.relative_to(self.reference_tif_dir)
+        if filetype == 'tif':
+            path_reference_copy = self.save_tif_dir / stem.with_suffix('.reference.tif')
+            path_target_copy = self.save_tif_dir / stem.with_suffix('.target.tif')
+        elif filetype == 'las':
+            path_reference = self.reference_las_dir / stem.with_suffix('.las')
+            path_target = self.target_las_dir / stem.with_suffix('.las')
+            path_reference_copy = self.save_las_dir / stem.with_suffix('.reference.las')
+            path_target_copy = self.save_las_dir / stem.with_suffix('.target.las')
+        else:
+            raise ValueError('Input must be tif or las file.')
 
-    try:
-        shutil.copyfile(path_reference, path_reference_copy)
-        shutil.copyfile(path_target, path_target_copy)
-    except shutil.SameFileError:
-        log.error('Source and destination represents the same file.')
-    except IsADirectoryError:
-        log.error('Destination is a directory.')
-    except PermissionError:
-        log.error('Permission denied.')
+        path_reference_copy.parent.mkdir(exist_ok=True, parents=True)
+        path_target_copy.parent.mkdir(exist_ok=True, parents=True)
 
+        try:
+            shutil.copyfile(path_reference, path_reference_copy)
+            shutil.copyfile(path_target, path_target_copy)
+        except shutil.SameFileError:
+            log.error('Source and destination represents the same file.')
+        except IsADirectoryError:
+            log.error('Destination is a directory.')
+        except PermissionError:
+            log.error('Permission denied.')
 
-def compare(args):
-    """
-    Compare reference and target.
-    :param args: path_reference, path_target
-    :return: whether the building is changed
-    """
-    path_reference, path_target, reference_dir, target_dir, save_dir, metrics = args
-    difference_array = difference(path_reference, path_target)
-    changed_ = changed(difference_array, **metrics)
+    def compare(self, args):
+        """
+        Compare reference and target.
+        :param args: path_reference, path_target
+        :return: whether the building is changed
+        """
+        path_reference, path_target = args
+        difference_array = self.difference(path_reference, path_target)
+        changed_ = self.changed(difference_array)
 
-    if True in changed_.values():
-        copy_tif(path_reference, path_target, reference_dir, target_dir, save_dir)
-        return changed_, path_reference
+        if True in changed_.values():
+            self.copy_file(path_reference, path_target, filetype='tif')
+            self.copy_file(path_reference, path_target, filetype='las')
+
+            return changed_, path_reference
 
 
 @hydra.main(config_path='conf', config_name='config')
@@ -124,25 +148,26 @@ def multi_run(cfg: DictConfig):
     Compare the difference with multi-processing.
     :return: None
     """
-    reference_dir = Path(cfg.compare.reference_dir)
-    target_dir = Path(cfg.compare.target_dir)
+    reference_dir = Path(cfg.compare.reference_tif_dir)
+    target_dir = Path(cfg.compare.target_tif_dir)
     reference_paths = reference_dir.rglob('*' + 'tif')
 
     args = []
     for path_reference in reference_paths:
         path_target = target_dir / path_reference.relative_to(reference_dir).with_suffix('.tif')
-        args.append((path_reference, path_target, cfg.compare.reference_dir, cfg.compare.target_dir,
-                     cfg.compare.save_dir, cfg.compare.metrics))
+        args.append((path_reference, path_target))
 
     pool = multiprocessing.Pool(processes=cfg.threads if cfg.threads else multiprocessing.cpu_count())
 
+    # initialise
+    raster_comparison = instantiate(cfg.compare)
+
     # https://stackoverflow.com/a/40133278
     counter = 0
-    for r in tqdm(pool.imap_unordered(compare, args), total=len(args)):
+    for r in tqdm(pool.imap_unordered(raster_comparison.compare, args), total=len(args)):
         if r is not None:
             counter += 1
-            if cfg.compare.verbose:
-                log.info(str(r[0]) + ' for ' + str(r[1]))
+            log.debug(str(r[0]) + ' for ' + str(r[1]))
     log.info(f' {counter} / {len(args)} has changed.')
 
 
